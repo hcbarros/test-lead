63bb733c9b9aebc3baabde3c48ff99b609de2bcd
hcbarros <hcb@a.recife.ifpe.edu.br>
2020-11-27 20:51:37 -0300

    commit

---
+++ src/main/java/br/com/lead/TestLeadApplication.java
@@ -0,0 +1,13 @@
+package br.com.lead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLeadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lead/TestLeadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
