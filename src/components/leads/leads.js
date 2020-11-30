fc2ad6237033f5f5fe07cf7df73d730020944fbf
hcbarros <hcb@a.recife.ifpe.edu.br>
2020-11-30 00:47:08 -0300

    commit

--- src/components/leads/leads.js
+++ src/components/leads/leads.js
@@ -33,56 +33,58 @@ const Leads = () => {
 
             <div>
 
-            <div id={!showTable ? 'loader-leads' : 'img-hide'}>
-                 <img src={loader} className="img-loader" alt="loader"></img>
-            </div>     
+                <div id={!showTable ? 'loader-leads' : 'img-hide'}>
+                    <img src={loader} className="img-loader" alt="loader"></img>
+                </div>     
 
-           {showTable && 
-                <table className="tableLeads">
-                    <caption>Leads</caption>                    
-                        <tr className="tableLeads">
-                            <th className="tableLeads">Id</th>
-                            <th className="tableLeads">Nome</th>
-                            <th className="tableLeads">Email</th>
-                            <th className="tableLeads">Telefone</th> 
-                            <th className="tableLeads">Criado em</th>
-                            <th className="tableLeads">Alterado em</th>
-                            <th className="tableLeads"></th> 
-                            <th className="tableLeads"></th>                   
-                        </tr>                    
-                    
-                        {leads.map((item) => (
-                        <tr key={item.id} className="tableLeads">
-                            <td className="tableLeads">{item.id}</td>
-                            <td className="tableLeads">{item.nome}</td>
-                            <td className="tableLeads">${item.email}</td>
-                            <td className="tableLeads">{item.telefone}</td>
-                            <td className="tableLeads">{item.criadoEm}</td>
-                            <td className="tableLeads">{item.atualizadoEm}</td>
-                            <td className="tableLeads">
-                                <a>
-                                    <img className="imgLeads" src={edit} alt="Imagem de editar"/>
-                                </a>
-                            </td>
-                            <td className="tableLeads">
-                                <a>
-                                    <img className="imgLeads" src={del} alt="Imagem da lixeira"/>
-                                </a>
-                            </td>                             
-                        </tr>
-                        ))}
-                   
+                <div id="container-table">
 
-                    {/* <button
-                            type="button"
-                            onClick={() => handleSubmit()}                        
-                            >
-                            Telefone
-                            </button>         */}
+                    {showTable && 
+                        <table className="tableLeads">
+                            <caption>Leads</caption>                    
+                                <tr className="tableLeads">
+                                    <th className="tableLeads">Id</th>
+                                    <th className="tableLeads">Nome</th>
+                                    <th className="tableLeads">Email</th>
+                                    <th className="tableLeads">Telefone</th> 
+                                    <th className="tableLeads">Criado em</th>
+                                    <th className="tableLeads">Alterado em</th>
+                                    <th className="tableLeads"></th> 
+                                    <th className="tableLeads"></th>                   
+                                </tr>                    
+                            
+                                {leads.map((item) => (
+                                <tr key={item.id} className="tableLeads">
+                                    <td className="tableLeads">{item.id}</td>
+                                    <td className="tableLeads">{item.nome}</td>
+                                    <td className="tableLeads">${item.email}</td>
+                                    <td className="tableLeads">{item.telefone}</td>
+                                    <td className="tableLeads">{item.criadoEm}</td>
+                                    <td className="tableLeads">{item.atualizadoEm}</td>
+                                    <td className="tableLeads">
+                                        <a>
+                                            <img className="imgLeads" src={edit} alt="Imagem de editar"/>
+                                        </a>
+                                    </td>
+                                    <td className="tableLeads">
+                                        <a>
+                                            <img className="imgLeads" src={del} alt="Imagem da lixeira"/>
+                                        </a>
+                                    </td>                             
+                                </tr>
+                                ))}
+                        
 
+                            {/* <button
+                                    type="button"
+                                    onClick={() => handleSubmit()}                        
+                                    >
+                                    Telefone
+                                    </button>         */}
 
+                        </table>}
 
-                </table>}
+                    </div>
                                 
             </div>
         );    
